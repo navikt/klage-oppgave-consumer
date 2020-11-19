4e02922199b4a87289e86bb56543e6c2686cd90a
Andreas Jonsson <andreas.jonsson@nav.no>
2020-11-19 21:10:32 +0100

    Some logging for testing.

--- src/main/kotlin/no/nav/klage/oppgave/repositories/OppgaveRepository.kt
+++ src/main/kotlin/no/nav/klage/oppgave/repositories/OppgaveRepository.kt
@@ -1,11 +1,18 @@
 package no.nav.klage.oppgave.repositories
 
+import no.nav.klage.oppgave.utils.getLogger
 import org.springframework.stereotype.Repository
 
 @Repository
 class OppgaveRepository {
+
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        private val logger = getLogger(javaClass.enclosingClass)
+    }
+
     fun storeHjemmelInMetadata(oppgaveId: Long, hjemmel: String) {
-        TODO("Not yet implemented")
+        logger.debug("Dry run: storeHjemmelInMetadata. OppgaveId: {}, hjemmel: {}", oppgaveId, hjemmel)
     }
 }
 // Fetch batch
