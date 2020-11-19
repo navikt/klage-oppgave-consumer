671c71ce33cdf0035c8d6f0226d6799afb0eb4dc
Richard Borge <richard.borge@nav.no>
2020-11-19 12:08:05 +0100

    Setting up bulk fetching

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.cloud:spring-cloud-starter-sleuth:$springSleuthVersion")
 
     implementation("io.micrometer:micrometer-registry-prometheus")
